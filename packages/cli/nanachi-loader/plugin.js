9b121fbc22cfa1ea76a978889839031f0969ff9b
blue.yang <blue.yang@qunar.com>
2021-04-30 13:57:42 +0800

    feat: process common functions before compile

--- packages/cli/nanachi-loader/plugin.js
+++ packages/cli/nanachi-loader/plugin.js
@@ -72,6 +72,25 @@ function rebuildManifest(manifestJson, quickPageDisplayConifg) {
     });
     return manifestJson;
 }
+function writeInternalCommonRuntime() {
+    const cwd = process.cwd();
+    const code = `
+    export function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+    
+    export function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+    
+    export function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+    
+    export function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+    
+    export function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+    `.trim();
+    const writeDistFilePath = process.env.NANACHI_CHAIK_MODE === 'CHAIK_MODE'
+        ? path_1.default.join(cwd, '../../dist', 'internal/runtimecommon.js')
+        : this.distCommonPath;
+    fs_extra_1.default.ensureFileSync(writeDistFilePath);
+    fs_extra_1.default.writeFileSync(writeDistFilePath, code);
+}
 class NanachiWebpackPlugin {
     constructor({ platform = 'wx', compress = false, beta = false, betaUi = false } = {}) {
         this.timer = new timer_1.default();
@@ -102,6 +121,9 @@ class NanachiWebpackPlugin {
             callback();
         }));
         compiler.hooks.beforeCompile.tapAsync(id, (compilation, callback) => __awaiter(this, void 0, void 0, function* () {
+            if (process.env.NANACHI_CHAIK_MODE === 'CHAIK_MODE') {
+                writeInternalCommonRuntime();
+            }
             callBeforeCompileFn(nanachiUserConfig);
             callBeforeCompileOnceFn(nanachiUserConfig);
             callback();
--- src/cli/nanachi-loader/plugin.ts
+++ src/cli/nanachi-loader/plugin.ts
@@ -83,6 +83,29 @@ function rebuildManifest(manifestJson:object, quickPageDisplayConifg:object) {
     return manifestJson;
 }
 
+function writeInternalCommonRuntime() {
+    const cwd = process.cwd();
+    const code = `
+    export function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+    
+    export function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+    
+    export function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+    
+    export function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+    
+    export function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+    `.trim();
+
+    const writeDistFilePath = process.env.NANACHI_CHAIK_MODE === 'CHAIK_MODE'
+        ? path.join(cwd, '../../dist', 'internal/runtimecommon.js')
+        : this.distCommonPath;
+
+        fs.ensureFileSync(writeDistFilePath);
+        fs.writeFileSync(writeDistFilePath, code);
+
+}
+
 class NanachiWebpackPlugin implements webpack.Plugin {
     private timer: Timer;
     private nanachiOptions: NanachiOptions;
@@ -137,6 +160,10 @@ class NanachiWebpackPlugin implements webpack.Plugin {
         });
 
         compiler.hooks.beforeCompile.tapAsync(id, async (compilation, callback) => {
+            // 这么做是因为服务于缓存编译
+            if (process.env.NANACHI_CHAIK_MODE === 'CHAIK_MODE') {
+                writeInternalCommonRuntime();
+            }
             callBeforeCompileFn(nanachiUserConfig);
             callBeforeCompileOnceFn(nanachiUserConfig);
             callback();
