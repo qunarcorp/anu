953227bfbabcea45e6e6eb65023b87533969d4e4
peacejj <18829349799@163.com>
2023-04-25 14:15:10 +0800

    chore: Version to 1.8.19

--- packages/cli/tasks/chaikaMergeTask/isMutilePack.d.ts
+++ packages/cli/tasks/chaikaMergeTask/isMutilePack.d.ts
@@ -1,3 +1,3 @@
 declare const isMultiple: () => any;
 export default isMultiple;
-export declare const getMultiplePackDirPrefix: () => "wx" | "qq" | "ali" | "bu" | "tt" | "quick" | "h5" | "360" | "";
+export declare const getMultiplePackDirPrefix: () => "" | "wx" | "qq" | "ali" | "bu" | "tt" | "quick" | "h5" | "360";
